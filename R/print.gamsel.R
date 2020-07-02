53bee4853dda41e2ddb4dd530d7199aaf7fa7ba8
Stathis Gennatas <gennatas@gmail.com>
2020-07-02 03:11:46 -0700

    make print work for single lambda / ensure data.frame

--- R/print.gamsel.R
+++ R/print.gamsel.R
@@ -1,7 +1,13 @@
-print.gamsel=function(x,digits = max(3, getOption("digits") - 3),...){
-      cat("\nCall: ", deparse(x$call), "\n\n")
-      out=cbind(summarynz(x)[,c(3,1,2)],signif(x$dev.ratio,digits),signif(x$lambda,digits))
-      colnames(out)=c("NonZero","Lin","NonLin","%Dev","Lambda")
-      print(out,...)
-      invisible(out)
-    }
+print.gamsel <- function(x,
+                          digits = max(3, getOption("digits") - 3), ...) {
+  # cat("\nCall: ", deparse(x$call), "\n\n")
+  # out <- cbind(summarynz(x)[, c(3, 1, 2)], 
+  #              signif(x$dev.ratio, digits), 
+  #              signif(x$lambda, digits))
+  out <- data.frame(matrix(summarynz(x)[, c(3, 1, 2)], length(x$lambdas)),
+                    signif(x$dev.ratio, digits),
+                    signif(x$lambda, digits))
+  colnames(out) <- c("NonZero", "Lin", "NonLin", "%Dev", "Lambda")
+  print(out, ...)
+  invisible(out)
+}
