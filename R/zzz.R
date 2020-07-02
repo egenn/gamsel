642cb71f73358085e5c5cc15709c9c3d18c5d248
Stathis Gennatas <gennatas@gmail.com>
2020-07-02 04:52:26 -0700

    cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 # zzz.R
 # ::gamsel::
 
-gamsel.version <- packageVersion("gamsel")
 
 .onAttach <- function(libname, pkgname) {
   
+  gamsel.version <- packageVersion(pkgname)
   packageStartupMessage(paste0("  ~ ", pkgname, " ", gamsel.version,
                                ": Welcome, ", Sys.getenv("USER")))
 }
